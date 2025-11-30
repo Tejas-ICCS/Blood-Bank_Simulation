f06970c26c148da6984bd1f3bc1813a662b2c47f
Tejas Devendra Devgharkar <tejasdevgharkar12@gmail.com>
2025-11-30 14:09:26 +0530

    Initial Commit

--- src/main/java/LoginPack/AdminLogin.java
+++ src/main/java/LoginPack/AdminLogin.java
@@ -1,4 +1,17 @@
 package LoginPack;
 
-public class AdminLogin {
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import org.springframework.http.HttpRequest;
+
+import java.net.http.HttpResponse;
+
+@WebServlet("/admin-login")
+public class AdminLogin extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpRequest request, HttpResponse response){
+        String username=request.getParameter("username");
+        String password=request.getParameter("password");
+    }
 }
